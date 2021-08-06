f7ce1fdaf555ee02dc4db8ea811b29ea532c9eac
zeketian <zeke.tian@outlook.com>
2021-08-06 18:25:41 +0800

    update: 3, add solution2

--- src/array/_3_LongestSubstringWithoutRepeatingCharacters.java
+++ src/array/_3_LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.HashMap;
+
 /**
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/
  * 题目描述：给定一个字符串 s，找到一个最长的不含重复字符的子串（连续）。如果能找到，则返回该子串的长度；否则，返回 0。
@@ -32,7 +34,7 @@ public class _3_LongestSubstringWithoutRepeatingCharacters {
         //        String s = "pwwkew";
         String s = "";
 
-        _3Solution solution = new _3Solution();
+        _3Solution1 solution = new _3Solution1();
 
         int len = solution.lengthOfLongestSubstring(s);
 
@@ -40,7 +42,10 @@ public class _3_LongestSubstringWithoutRepeatingCharacters {
     }
 }
 
-class _3Solution {
+/**
+ * 解法一：使用一个数组标记是否存在重复的字符
+ */
+class _3Solution1 {
     public int lengthOfLongestSubstring(String s) {
         int length = 0; // 满足条件的最长子串的长度
         int left = 0; // 左指针，子串的左边界
@@ -67,3 +72,40 @@ class _3Solution {
         return length;
     }
 }
+
+/**
+ * 解法二：使用 Map 标记是否存在重复的字符
+ */
+class _3Solution2 {
+    
+    public int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>(); // 使用 map 存储不重复的字符
+        int maxLen = -1; // 最长的不重复的子串的长度
+        int left = 0; // 最长的不重复的子串中第一个字符在 s 中的下标
+        
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            
+            if (null != map.get(c)) {
+                // 当发现有重复的，则更新 maxLen、map、 left
+                maxLen = Math.max(map.size(), maxLen);
+
+                // 删除 map 中存储的元素，即 s[left, idx] 之间的元素，从而便于存放 s[i]
+                int idx = map.get(c);
+                for (int j = left; j <= idx; ++j) {
+                    map.remove(s.charAt(j));
+                }
+                
+                left = idx + 1;
+            }
+
+            map.put(c, i);
+        }
+
+        maxLen = Math.max(map.size(), maxLen);
+
+        return maxLen;
+    }
+}
+
+
