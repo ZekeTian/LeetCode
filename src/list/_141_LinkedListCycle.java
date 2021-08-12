806c7c7f71dc00ca05e8f48afebecf80c3744b1d
zeketian <zeke.tian@outlook.com>
2021-08-12 13:04:18 +0800

    update: 141 (add solution2)

--- src/list/_141_LinkedListCycle.java
+++ src/list/_141_LinkedListCycle.java
@@ -10,11 +10,11 @@ public class _141_LinkedListCycle {
 
     public static void main(String[] args) {
         // test case1, output: true
-        //        int[] nums = {3, 2, 0, -4};
-        //        ListNode head = ListUtil.createList(nums);
-        //        ListNode tail = ListUtil.lastElement(head);
-        //        ListNode node = ListUtil.get(head, 1);
-        //        tail.next = node;
+        int[] nums = { 3, 2, 0, -4 };
+        ListNode head = ListUtil.createList(nums);
+        ListNode tail = ListUtil.lastElement(head);
+        ListNode node = ListUtil.get(head, 1);
+        tail.next = node;
 
         // test case2, output: true
         //        int[] nums = { 1, 2 };
@@ -24,8 +24,8 @@ public class _141_LinkedListCycle {
         //        tail.next = node;
 
         // test case3, output: false
-        int[] nums = { 1 };
-        ListNode head = ListUtil.createList(nums);
+        //        int[] nums = { 1 };
+        //        ListNode head = ListUtil.createList(nums);
 
         _141Solution1 solution = new _141Solution1();
 
@@ -58,3 +58,27 @@ class _141Solution1 {
     }
 }
 
+/**
+ * 解法二：使用快慢指针，快指针一次走两步，慢指针一次走一步
+ */
+class _141Solution2 {
+    public boolean hasCycle(ListNode head) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode slow = dummyHead;
+        ListNode fast = dummyHead;
+
+        // 慢指针 slow 走一步，快指针 fast 走两步
+        while (null != slow.next && null != fast.next && null != fast.next.next) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            // 快慢指针相遇，则说明有环
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
