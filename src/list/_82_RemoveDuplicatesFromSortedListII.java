944532e36d2e41dc1d0e1e7a5823a33fce1a225b
zeketian <zeke.tian@outlook.com>
2021-09-02 12:47:04 +0800

    update: 82 (add solution2)

--- src/list/_82_RemoveDuplicatesFromSortedListII.java
+++ src/list/_82_RemoveDuplicatesFromSortedListII.java
@@ -21,18 +21,18 @@ public class _82_RemoveDuplicatesFromSortedListII {
 
     public static void main(String[] args) {
         // test case 1, output: 1,2,5
-        //        int[] nums = { 1, 2, 3, 3, 4, 4, 5 };
-        //        ListNode head = ListUtil.createList(nums);
+        int[] nums = { 1, 2, 3, 3, 4, 4, 5 };
 
         // test case 2, output: 2, 3
         //        int[] nums = { 1, 1, 1, 2, 3 };
-        //        ListNode head = ListUtil.createList(nums);
 
         // test case 3, output: null
-        int[] nums = { 1, 1, 1 };
+        //        int[] nums = { 1, 1, 1 };
+
         ListNode head = ListUtil.createList(nums);
 
-        _82Solution1 solution = new _82Solution1();
+        //        _82Solution1 solution = new _82Solution1();
+        _82Solution2 solution = new _82Solution2();
 
         head = solution.deleteDuplicates(head);
         ListUtil.print(head);
@@ -70,3 +70,33 @@ class _82Solution1 {
     }
 }
 
+/**
+ * 解法二：不使用 Map，一次遍历删除
+ */
+class _82Solution2 {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode pre = dummyHead;
+        ListNode cur = dummyHead.next;
+
+        while (null != cur) {
+            // 找到第一个与 cur 不相同的元素
+            ListNode next = cur.next;
+            boolean isRepeat = false; // 标记 cur 是否重复
+            while (null != next && next.val == cur.val) {
+                next = next.next;
+                isRepeat = true;
+            }
+
+            if (isRepeat) {
+                pre.next = next; // cur 有重复，则 pre 直接和第一个与 cur 不重重复的元素相连，相当于把所有 cur 全部删除
+            } else {
+                pre = cur; // cur 不重复，则 pre 向箭移动
+            }
+            cur = next;
+        }
+
+        return dummyHead.next;
+    }
+}
