ab8bf38d7f809f80ae1eb0dd00dbe8d9f2ce9b0a
zeketian <zeke.tian@outlook.com>
2021-11-29 14:22:03 +0800

    update: 63 (add solution3)

--- src/dynamicprogramming/_63_UniquePathsII.java
+++ src/dynamicprogramming/_63_UniquePathsII.java
@@ -37,7 +37,11 @@ public class _63_UniquePathsII {
         // test case 2, output: 1
 //        int[][] obstacleGrid = {{0,1},{0,0}};
         
-        _63Solution1 solution = new _63Solution1();
+//        _63Solution1 solution = new _63Solution1();
+        
+//        _63Solution2 solution = new _63Solution2();
+
+        _63Solution3 solution = new _63Solution3();
         
         System.out.println(solution.uniquePathsWithObstacles(obstacleGrid));
     }
@@ -140,4 +144,47 @@ class _63Solution2 {
     }
     
 }
+
+/**
+ * 解法三：自底向上动态规划
+ */
+class _63Solution3 {
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] memo = new int[m][n];
+        
+        if (1 == obstacleGrid[m - 1][n - 1]) {
+            return 0;
+        }
+        
+        memo[m - 1][n - 1] = 1;
+        for (int i = n - 2; i >= 0; --i) {
+            if (1 == obstacleGrid[m - 1][i]) {
+                memo[m - 1][i] = 0;
+            } else {
+                memo[m - 1][i] = memo[m - 1][i + 1];
+            }
+        }
+        
+        for (int i = m - 2; i >= 0; --i) {
+            if (1 == obstacleGrid[i][n - 1]) {
+                memo[i][n - 1] = 0;
+            } else {
+                memo[i][n - 1] = memo[i + 1][n - 1];
+            }
+            for (int j = n - 2; j >= 0; --j) {
+                if (1 == obstacleGrid[i][j]) {
+                    memo[i][j] = 0;
+                } else {
+                    memo[i][j] = memo[i + 1][j] + memo[i][j + 1];
+                }
+            }
+        }
+        
+        return memo[0][0];
+    }
+}
+
 
