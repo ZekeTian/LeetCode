0609ec290dda4008d37593cd7cf2dc1c5a447eb0
zeketian <zeke.tian@outlook.com>
2021-12-04 22:52:41 +0800

    update: 494 (add solution2)

--- src/dynamicprogramming/_494_TargetSum.java
+++ src/dynamicprogramming/_494_TargetSum.java
@@ -35,14 +35,20 @@ public class _494_TargetSum {
 
     public static void main(String[] args) {
         // test case 1, output: 5
-//        int[] nums = {1,1,1,1,1};
-//        int target = 3;
+        int[] nums = {1,1,1,1,1};
+        int target = 3;
         
         // test case 2, output: 1
-        int[] nums = {1};
-        int target = 1;
+//        int[] nums = {1};
+//        int target = 1;
         
-        _494Solution1 solution = new _494Solution1();
+        // test case 3, output: 256
+//        int[] nums = {0, 0, 0, 0, 0, 0, 0, 0, 1};
+//        int target = 1;
+        
+//        _494Solution1 solution = new _494Solution1();
+
+        _494Solution2 solution = new _494Solution2();
         
         System.out.println(solution.findTargetSumWays(nums, target));
     }
@@ -58,7 +64,7 @@ class _494Solution1 {
     
     // 返回数组 nums[0...index] 组成和为 target 的组合个数
     private int tryFindTargetSumWays(int index, int target) {
-        if (index < 0) {
+        if (index < 0) { // index = 0 时结束递归，但是要对 nums[0] = target = 0 这种情况进行特殊处理，因为 +0、-0 在此题中是不同的
             return (0 == target ? 1 : 0);
         }
         
@@ -71,6 +77,7 @@ class _494Solution1 {
                 + tryFindTargetSumWays(index - 1, target - nums[index]);
         
         memo.get(index).put(target, res);
+        
         return res;
     }
     
@@ -82,5 +89,45 @@ class _494Solution1 {
         this.nums = nums;
         
         return tryFindTargetSumWays(nums.length - 1, target);
+    }
+}
+
+/**
+ * 解法二：自底向上动态规划
+ */
+class _494Solution2 {
+    
+    public int findTargetSumWays(int[] nums, int target) {
+        // 计算出 nums 数组的元素之和
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+        
+        int[][] memo = new int[nums.length][2 * sum + 1]; // memo[i][j] 表示 nums[0...i] 数组组成和为 j 的组合个数
+        // 初始化。因为 nums[0] 有正负两种情况，所以初始化时有两种选择
+        memo[0][nums[0] + sum] = 1; // nums[0] 选择负
+        memo[0][-nums[0] + sum] = 1; // nums[0] 选择正
+        if (0 == nums[0]) { // 因为 nums[0] = 0 时， +0、-0 无法区分，所以单独处理
+            memo[0][sum] = 2; // 当 nums[0] = 0 时，+0 和 -0 是两种不同的情况，因此 memo[0][sum] 设置为 2
+        }
+        
+        for (int i = 1; i < nums.length; ++i) {
+            for (int j = 0; j <= 2 * sum; ++j) {
+                int res = 0;
+                if (j - nums[i] >= 0) {
+                    res += memo[i - 1][j - nums[i]]; // nums[i] 选择正时
+                }
+                if (j + nums[i] <= 2 * sum) {
+                    res += memo[i - 1][j + nums[i]]; // nums[i] 选择负时
+                }
+                memo[i][j] = res;
+            }
+        }
+
+        return memo[nums.length - 1][target + sum];
     }
 }
