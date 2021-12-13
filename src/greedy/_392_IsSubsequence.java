6af932a9a90b7b29a76709d9f162c74f11d82a2e
zeketian <zeke.tian@outlook.com>
2021-12-13 10:21:55 +0800

    update: 392 (add solution2)

--- src/greedy/_392_IsSubsequence.java
+++ src/greedy/_392_IsSubsequence.java
@@ -24,20 +24,26 @@ public class _392_IsSubsequence {
     
     public static void main(String[] args) {
         // test case1, true
-//        String s = "abc";
-//        String t = "ahbgdc";
-        
-        // test case2, false
-        String s = "axc";
+        String s = "abc";
         String t = "ahbgdc";
         
-        _392Solution solution = new _392Solution();
+        // test case2, false
+//        String s = "axc";
+//        String t = "ahbgdc";
+        
+//      _392Solution1 solution = new _392Solution1();
+        
+        _392Solution2 solution = new _392Solution2();
+        
+        
         System.out.println(solution.isSubsequence(s, t));
     }
 }
 
-
-class _392Solution {
+/**
+ * 解法一：贪心算法
+ */
+class _392Solution1 {
     public boolean isSubsequence(String s, String t) {
         int sIdx = 0;
         int tIdx = 0;
@@ -53,4 +59,24 @@ class _392Solution {
     }
 }
 
-
+/**
+ * 解法二：动态规划（编辑距离问题）
+ */
+class _392Solution2 {
+    
+    public boolean isSubsequence(String s, String t) {
+        int[][] memo = new int[s.length() + 1][t.length() + 1]; // memo[i][j] 表示字符串 s[0...i-1] 与 t[0...j-1] 的公共子序列的长度
+        
+        for (int i = 1; i <= s.length(); ++i) {
+            for (int j = 1; j <= t.length(); ++j) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    memo[i][j] = memo[i - 1][j - 1] + 1; // s[i - 1] = t[j - 1]，则在之前长度的基础上加上当前字符（即 +1）
+                } else {
+                    memo[i][j] = memo[i][j - 1]; // s[i - 1] != t[j - 1]，则字符串 t 删除 t[j - 1]，删除后的结果即为当前的结果 
+                }
+            }
+        }
+        
+        return memo[s.length()][t.length()] == s.length();
+    }
+}
