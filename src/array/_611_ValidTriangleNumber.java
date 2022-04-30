d1983dc1f4ba76cca699a55255044c9ef8c35400
zeketian <zeke.tian@outlook.com>
2022-04-30 15:28:20 +0800

    add: 611

---
+++ src/array/_611_ValidTriangleNumber.java
@@ -0,0 +1,88 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/valid-triangle-number/
+ * 
+ * 题目描述：给定一个包含非负整数的数组 nums ，返回其中可以组成三角形三条边的三元组个数。
+ * 
+ * 限制条件：
+ *  （1）1 <= nums.length <= 1000
+ *  （2）0 <= nums[i] <= 1000
+ *  
+ * 示例：
+ *  示例 1
+ *      输入: nums = [2,2,3,4]
+ *      输出: 3
+ *      解释:有效的组合是: 
+ *      2,3,4 (使用第一个 2)
+ *      2,3,4 (使用第二个 2)
+ *      2,2,3
+ *
+ * 示例 2
+ *      输入: nums = [4,2,3,4]
+ *      输出: 4
+ * 
+ */
+public class _611_ValidTriangleNumber {
+
+    public static void main(String[] args) {
+        // test case1, output: 3
+//        int[] nums = { 2, 2, 3, 4 };
+        
+        // test case2, output: 4
+        int[] nums = { 4, 2, 3, 4 };
+        
+        
+        _611Solution1 soltuion = new _611Solution1();
+        
+        System.out.println(soltuion.triangleNumber(nums));
+        
+    }
+}
+
+/**
+ * 解法一：排序 + 二分搜索
+ *       对于三角形的三条边 a、b、c，其必须满足：
+ *          a + b > c
+ *          a + c > b
+ *          b + c > a
+ *       假设，a <= b <= c，则 a + c > b 和 b + c > a 一定成立，故现在只需要满足 a + b > c 即可。
+ *       为了满足 a <= b <= c，先对 nums 进行排序，然后用 i、j 两个指针表示 a、b 的位置，然后在 [j+1, nums.length-1] 之间寻找最后一个
+ *       小于 a + b 的数字 c。假设数字 c 的下标是 k ，则满足 a + b > c 的总共有 k - j 组。
+ *       然后不断移动 i、j，直到找到满足所有条件的 a、b、c。
+ */
+class _611Solution1 {
+    
+    public int triangleNumber(int[] nums) {
+        if (null == nums || nums.length < 3) {
+            return 0;
+        }
+        
+        Arrays.sort(nums);
+        
+        int count = 0; // 有效的三元组个数
+        for (int i = 0; i < nums.length - 2 /* 留两个数 b、c */ ; ++i) { // nums[i] 表示 a
+            for (int j = i + 1; j < nums.length - 1 /* 留一个数 c */ ; ++j) { // nums[j] 表示 b 
+                // 在 [j + 1, nums.length - 1] 之间寻找最后一个小于 nums[i] + nums[j] 的数字 c（下标为 k）
+                int left = j + 1, right = nums.length - 1, k = j;
+                while (left <= right) {
+                    int mid = left + (right - left) / 2;
+                    if (nums[mid] < nums[i] + nums[j]) {
+                        k = mid;
+                        left = mid + 1;
+                    } else {
+                        right = mid - 1;
+                    }
+                }
+                
+                count = count + k - j; // nums[j+1, k] 之间的数字都可以与 nums[i]、nums[j] 组成合法的三元组
+            }
+        }
+        
+        return count;
+    }
+}
+
+
