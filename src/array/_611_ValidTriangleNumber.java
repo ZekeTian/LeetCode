bc67b37143d53544a9a061f93e1d4a36e5a0467e
zeketian <zeke.tian@outlook.com>
2022-04-30 15:37:06 +0800

    update: 611 (add solution2)

--- src/array/_611_ValidTriangleNumber.java
+++ src/array/_611_ValidTriangleNumber.java
@@ -35,7 +35,9 @@ public class _611_ValidTriangleNumber {
         int[] nums = { 4, 2, 3, 4 };
         
         
-        _611Solution1 soltuion = new _611Solution1();
+//        _611Solution1 soltuion = new _611Solution1();
+        
+        _611Solution2 soltuion = new _611Solution2();
         
         System.out.println(soltuion.triangleNumber(nums));
         
@@ -86,3 +88,37 @@ class _611Solution1 {
 }
 
 
+/**
+ * 解法二：排序 + 双指针
+ *       与解法一样，先对数组进行排序。之后，固定最大的数字 c，假设 c 的下标是 i，然后在 [0, i-1] 之间寻找所有可能的 a、b。
+ *       假设指针 left、right 分别代表 a、b 的位置，
+ *          如果 nums[left] + nums[right] > c，则 nums[left+1] + nums[right] > c、nums[left+2] + nums[right] > c、
+ *       nums[left+3] + nums[right] > c、……、nums[right-1] + nums[right] > c 都成立，即总共有 right - left 组。之后，
+ *       再将 right 左移，计算下一组。
+ *          如果 nums[left] + nums[right] <= c，则需要增大 nums[left]，即 left 右移。
+ */
+class _611Solution2 {
+    
+    public int triangleNumber(int[] nums) {
+        if (null == nums || nums.length < 3) {
+            return 0;
+        }
+        
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = nums.length - 1; i >= 2; --i) { // nums[i] 表示 a、b、c 中最大的数字 c
+            int left = 0, right = i - 1; // nums[left]、nums[right] 分别表示 a、b
+            
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    count = count + right - left; // nums[left...right-1] 之间的数字 和 nums[right] 、nums[i] 都可以组成有效的三元组
+                    --right;
+                } else {
+                    ++left;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
