8f3daa8c18a6b6bd51576c5c1f810161669133bc
zeketian <zeke.tian@outlook.com>
2022-06-07 09:31:32 +0800

    update: 378 (add solution3)

--- src/queue/_378_KthSmallestElementInASortedMatrix.java
+++ src/queue/_378_KthSmallestElementInASortedMatrix.java
@@ -37,18 +37,20 @@ public class _378_KthSmallestElementInASortedMatrix {
 
     public static void main(String[] args) {
         // test case1, output: 13
-        int[][] matrix = { { 1, 5, 9 }, 
-                           { 10, 11, 13 }, 
-                           { 12, 13, 15 } };
-        int k = 8;
+//        int[][] matrix = { { 1, 5, 9 }, 
+//                           { 10, 11, 13 }, 
+//                           { 12, 13, 15 } };
+//        int k = 8;
 
         // test case2, output: -5
-//        int[][] matrix = { { -5 } };
-//        int k = 1;
+        int[][] matrix = { { -5 } };
+        int k = 1;
         
 //        _378Solution1 soltuion = new _378Solution1();
         
-        _378Solution2 soltuion = new _378Solution2();
+//        _378Solution2 soltuion = new _378Solution2();
+        
+        _378Solution3 soltuion = new _378Solution3();
         
         
         System.out.println(soltuion.kthSmallest(matrix, k));
@@ -117,4 +119,66 @@ class _378Solution2 {
         return maxHeap.peek();
     }
 
+}
+
+/**
+ * 解法三：二分搜索
+ *        1     5       9
+ *        10    11      13
+ *        12    13      15
+ * 因为矩阵 matrix 中的每行和每列元素均按升序排序，所以可以利用有序性进行二分搜索。
+ * 根据 matrix 有序的特点可知，matrix[0][0] 是最小值，matrix[n-1][n-1] 是最大值，现在需要在 matrix[0][0] ~ matrix[n-1][n-1] 之间
+ * 寻找一个值 target，使得 matrix 中小于等于 target 的元素个数刚好为 k（即 target 是 matrix 中第 k 小的元素）。
+ * 从上面的示例 matrix 中可以看出，矩阵对角线的左上部分中的元素是小于等于对角线的右下部分中的元素，利用该特点可以快速统计出矩阵中小于等于 target
+ * 的元素个数。
+ */
+class _378Solution3 {
+    
+    private int[][] matrix = null;
+    private int k = 0;
+    private int n = 0; // matrix 的长度
+    
+    // 统计出 matrix 中小于等于 target 的元素个数 count，然后判断 count 是否大于等于 k
+    private boolean check(int target) {
+        // matrix[i][j] 的初始位置在左下角
+        int i = n - 1;
+        int j = 0;
+        int count = 0;
+        
+        while (i >= 0 &&  j < n) {
+            if (matrix[i][j] <= target) {
+                count = count + i + 1; // matrix[i][j] 所在的第 j 列中，0~i 行的元素都会小于等于 target ，所以 count 需要加上 i + 1
+                ++j; // 继续统计下一列
+            } else {
+                --i; // 向下移动一行，减小 matrix[i][j]
+            }
+        }
+        
+        return count >= k;
+    }
+    
+    public int kthSmallest(int[][] matrix, int k) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        
+        this.matrix = matrix;
+        this.k = k;
+        this.n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (check(mid)) {
+                right = mid; // 小于等于 mid 的元素个数大于等于 k，则需要缩小 mid，因此左移 right，减小区间（但是，需要注意的是，不能丢弃 mid）
+            } else {
+                left = mid + 1; // 小于等于 mid 的元素个数小于 k，则需要增大 mid，因此右移 left，增大区间
+            }
+        }
+        
+        return left;
+    }
+    
 }
